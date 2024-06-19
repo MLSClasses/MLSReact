3ff47712f3873b657f3dfca8a1362f2f0887c457
Aditya Bahl <adityabahl12345@gmail.com>
2024-06-19 18:25:51 +0530

    Minor Changes 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //Dependencies
 import './App.scss';
 import 'boxicons/css/boxicons.min.css';
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 //components
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+// import Navbar from "./components/navbar";
+// import Footer from "./components/footer";
 //pages
 import Home from "./pages/home";
 import Contact from "./pages/contact_component";
@@ -16,7 +17,7 @@ import Login from "./pages/loginpage";
 // import Signup from "./pages/Signuppage";
 import Portal from "./pages/student/portal";
 import Chat from './pages/student/chat';
-import Whatsapp_icon from "./components/whatsapp_icon";
+import WhatsappIcon from "./components/whatsapp_icon";
 import StudentDashboard from "./pages/student/student_dashboard";
 import ACT_READING_WRITING from './blogs/act_reading_writing';
 
@@ -31,7 +32,7 @@ import CancellationPage from './pages/cancel_refund';
 function App() {
   return (
     <div class="body">
-      <Whatsapp_icon />
+      <WhatsappIcon />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -49,34 +50,31 @@ function App() {
           <Route path="/act_science" element={<ACT_READING_WRITING banner_img={act_science_img} headingText={act_science_heading} mainContent={act_science} conclusion={act_science_conclusion} />}></Route>
           <Route path="/act_english" element={<ACT_READING_WRITING banner_img={act_english_img} headingText={act_english_heading} mainContent={act_english} conclusion={act_english_conclusion} />}></Route>
           <Route path="/act_math" element={<ACT_READING_WRITING banner_img={act_math_img} headingText={act_math_heading} mainContent={act_math} conclusion={act_math_conclusion} />}></Route>
-          <Route path="/sat_reading_writing" element={<ACT_READING_WRITING feedback={false} banner_img={sat_r_w} headingText={sat_reading_writing_heading} mainContent={sat_reading_writing} conclusion={sat_reading_writing_conclusion} student_feedback={sat_reading_writing_feedback}/>}></Route>
-          <Route path="/sat_math" element={<ACT_READING_WRITING feedback={true} banner_img={sat_math_img} headingText={sat_math_heading} mainContent={sat_math} conclusion={sat_math_conclusion} student_feedback={sat_math_conclusion}/>}></Route>
+          <Route path="/sat_reading_writing" element={<ACT_READING_WRITING feedback={false} banner_img={sat_r_w} headingText={sat_reading_writing_heading} mainContent={sat_reading_writing} conclusion={sat_reading_writing_conclusion} student_feedback={sat_reading_writing_feedback} />}></Route>
+          <Route path="/sat_math" element={<ACT_READING_WRITING feedback={true} banner_img={sat_math_img} headingText={sat_math_heading} mainContent={sat_math} conclusion={sat_math_conclusion} student_feedback={sat_math_conclusion} />}></Route>
           {/* 👇️ only match this when no other routes match */}
           {/* <Route path="*" element={<PageNotFound />} /> */}
-          <Route path="/sat_workshop" element={<LandingPage/>}></Route>
-          <Route path="/cancellation_refund" element={<CancellationPage/>}></Route>
-          <Route path="/privacy" element={<Privacy/>}></Route>
-          <Route path="/shipping" element={<Shipping/>}></Route>
-          <Route path="/contact" element={<ContactPage/>}></Route>
-          <Route path="/terms" element={<Term/>}></Route>
+          <Route path="/sat_workshop" element={<LandingPage />}></Route>
+          <Route path="/cancellation_refund" element={<CancellationPage />}></Route>
+          <Route path="/privacy" element={<Privacy />}></Route>
+          <Route path="/shipping" element={<Shipping />}></Route>
+          <Route path="/contact" element={<ContactPage />}></Route>
+          <Route path="/terms" element={<Term />}></Route>
 
-          
+
         </Routes>
       </BrowserRouter>
     </div>
   );
 
 }
-
-
-
 export default App;
-function PageNotFound() {
-  window.location.href = "/";
-  return (
-    <div>
-      <h2>404 Page not found</h2>
-    </div>
-  );
-}
+// function PageNotFound() {
+//   window.location.href = "/";
+//   return (
+//     <div>
+//       <h2>404 Page not found</h2>
+//     </div>
+//   );
+// }
 
